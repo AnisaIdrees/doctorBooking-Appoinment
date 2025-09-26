28667e0d95c4cb7caeb42faa14c91405f40b31b9
AnisaIdrees <anisaidrees191@gmail.com>
2025-09-26 10:22:36 -0700

    add

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -6,7 +6,7 @@ function AboutSection() {
   return (
     <div>
       <HeaderBanner heading1={'About'}
-      bgImg={`https://cdn.dribbble.com/userupload/42271036/file/original-7343ade4c74b7b04f341cd0ddce462c5.gif`}
+        bgImg={`https://cdn.dribbble.com/userupload/42271036/file/original-7343ade4c74b7b04f341cd0ddce462c5.gif`}
         paragraph={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, praesentium.'} />
     </div>
   )
